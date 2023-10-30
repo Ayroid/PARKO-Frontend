0b293df0782fb0a2686150108bf39562cbc35c8c
Ayroid <ayushsk0000@gmail.com>
2023-10-30 18:39:54 +0530

    prettier refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,36 @@
-import { useState } from 'react'
-import './App.css'
-import Navigation from './components/Navigation'
-import Header from './components/Header';
-import Map from './components/home/Map';
-import Parkings from './components/home/Parking';
-import Login from './components/SignUp/Login';
-import Register from './components/SignUp/Register';
+// import { useState } from 'react'
+import "./App.css";
+import Navigation from "./components/Navigation";
+import Header from "./components/Header";
+import Map from "./components/home/Map";
+import Parkings from "./components/home/Parking";
+import Login from "./components/SignUp/Login";
+import Register from "./components/SignUp/Register";
 
-import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-
   return (
     <>
-     
-    <Router>
-      <Routes>
-        <Route path='/home' element={<div><Header/><Navigation/><Map/><Parkings/></div>}/>
-        <Route path='/' element={<Login/>}/>
-        <Route path='/register' element={<Register/>}/>
-      </Routes>
-    </Router>
-       
+      <Router>
+        <Routes>
+          <Route
+            path="/home"
+            element={
+              <div>
+                <Header />
+                <Navigation />
+                <Map />
+                <Parkings />
+              </div>
+            }
+          />
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
-import { Link,Route,Routes } from 'react-router-dom'
+import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <div className='bg-orange-500 p-5 pb-1 text-white font-Nunito font-bold'>
+    <div className="bg-orange-500 p-5 pb-1 text-white font-Nunito font-bold">
       <nav>
-        <ul className='flex justify-evenly'>
-          <li><Link to="/home">Home</Link></li>
-          <li><Link to="/history">About</Link></li>
-          <li><Link to="/tools">Tools</Link></li>
+        <ul className="flex justify-evenly">
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/history">About</Link>
+          </li>
+          <li>
+            <Link to="/tools">Tools</Link>
+          </li>
         </ul>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
