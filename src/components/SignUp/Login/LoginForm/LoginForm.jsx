1e4d6b57635f9863e96b72a57cfc32ef4c330c31
Ayroid <ayushsk0000@gmail.com>
2023-11-05 04:13:04 +0530

    login works

--- src/components/SignUp/Login/LoginForm/LoginForm.jsx
+++ src/components/SignUp/Login/LoginForm/LoginForm.jsx
@@ -1,71 +1,102 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import styles from "./LoginForm.module.css";
 import axios from "axios";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-const serverURL = "https://parko.studio/api/user/test";
+const serverURL = "http://localhost:3000";
+// const serverURL = "https://parko.studio";
+
+const otpRequestURL = serverURL + "/api/user/login/mail";
+const otpVerifyURL = serverURL + "/api/user/verify/mail";
 
 const LoginForm = () => {
   // ---------------------------- FORM VALIDATION ----------------------------
 
-  // useRef() Hooks to handle form data
-  const emailRef = useRef("");
-  const otpRef = useRef("");
-
   // useState() Hooks to handle form validation
-  const [emailValidated, setEmailValidated] = useState(false);
-  const [otpValidated, setOtpValidated] = useState(false);
+  const [email, setEmail] = useState("");
+  const [otp, setOtp] = useState("");
+  const [emailValidated, setEmailValidated] = useState(true);
+  const [otpValidated, setOTPValidated] = useState(true);
+  const [otpSent, setOTPSent] = useState(false);
 
   // Function to check if email is valid
   const checkEmailRef = () => {
-    const current = emailRef.current;
-    const value = emailRef.current.value;
+    console.log("EMAIL CALLED");
+    const value = email;
     if (
-      current !== null &&
+      value !== null &&
       value !== "" &&
       value.includes("@") &&
       value.includes(".")
     ) {
       setEmailValidated(true);
+      return true;
     } else {
       setEmailValidated(false);
+      return false;
     }
   };
 
   // Function to check if otp is valid
   const checkOTP = () => {
-    const current = otpRef.current;
-    const value = otpRef.current.value;
+    console.log("OTP CALLED");
+    const value = otp;
     if (
-      current !== null &&
+      value !== null &&
       value !== "" &&
       value.length === 6 &&
       Number.isInteger(parseFloat(value))
     ) {
-      setOtpValidated(true);
+      setOTPValidated(true);
+      return true;
     } else {
-      setOtpValidated(false);
-      console.log(Number.isInteger(parseFloat(value)))
+      setOTPValidated(false);
+      return false;
     }
   };
 
   // ---------------------------- FORM SUBMITTION ----------------------------
 
-  const handleSubmit = async (event) => {
+  const handleOTPRequest = async (event) => {
     try {
       event.preventDefault();
-      checkEmailRef();
-      checkOTP();
-      console.log("emailValidated:", emailValidated);
-      console.log("otpValidated:", otpValidated);
       const data = {
-        email: emailRef.current.value,
-        otpValue: otpRef.current.value,
+        email: email,
       };
-      if (emailValidated && otpValidated) {
-        console.log("data:", data);
-        const response = await axios.post(serverURL, data);
+
+      if (checkEmailRef()) {
+        console.log("Sending Email request");
+        const response = await axios.post(otpRequestURL, data);
+
+        if (response.status === 200) {
+          console.log("Login successful:", response.data);
+          setOTPSent(true);
+          toast.success("Login OTP sent successfully");
+        } else {
+          console.error("Login failed:", response.status);
+          setOTPSent(false);
+          toast.error("Login OTP failed");
+        }
+      }
+    } catch (error) {
+      console.error("Error sending Login request:", error);
+    }
+  };
+
+  const handleFormSubmit = async (event) => {
+    try {
+      event.preventDefault();
+      const data = {
+        email: email,
+        otpValue: otp,
+      };
+
+      console.log(data);
+
+      if (checkEmailRef() && checkOTP()) {
+        console.log("Sending Login request");
+        const response = await axios.post(otpVerifyURL, data);
 
         if (response.status === 200) {
           console.log("Login successful:", response.data);
@@ -93,34 +124,59 @@ const LoginForm = () => {
 
   return (
     <div>
-      <form className={form} onSubmit={handleSubmit}>
-        <div className={inputDiv}>
-          <input
-            className={input}
-            id="email"
-            type="email"
-            placeholder="EMAIL"
-            ref={emailRef}
-            required
-          />
-          {!emailValidated && (
-            <span className={errorMessage}>Invalid Email</span>
+      {!otpSent ? (
+        <form className={form} onSubmit={handleOTPRequest}>
+          <div className={inputDiv}>
+            <input
+              className={input}
+              id="email"
+              type="email"
+              placeholder="EMAIL"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              required
+            />
+            {!emailValidated && (
+              <span className={errorMessage}>Invalid Email</span>
+            )}
+          </div>
+          <button className={button}>SEND OTP</button>
+        </form>
+      ) : (
+        <form className={form} onSubmit={handleFormSubmit}>
+          <div className={inputDiv}>
+            <input
+              className={input}
+              id="email"
+              type="email"
+              placeholder="EMAIL"
+              value={email}
+              required
+              disabled
+            />
+          </div>
+
+          <div className={inputDiv}>
+            <input
+              className={input}
+              id="otpValue"
+              type="text"
+              placeholder="OTP"
+              maxLength={6}
+              value={otp}
+              onChange={(event) => setOtp(event.target.value)}
+            />
+            {!otpValidated && <span className={errorMessage}>Invalid OTP</span>}
+          </div>
+
+          {otpSent ? (
+            <button className={button}>SEND OTP</button>
+          ) : (
+            <button className={button}>LOGIN</button>
           )}
-        </div>
-        <div className={inputDiv}>
-          <input
-            className={input}
-            id="otpValue"
-            type="text"
-            placeholder="OTP"
-            maxLength={6}
-            ref={otpRef}
-          />
-          {!otpValidated && <span className={errorMessage}>Invalid OTP</span>}
-        </div>
-        <button className={button}>SEND OTP</button>
-        <div className={forgotPassword}>Don't have an account? Sign Up</div>
-      </form>
+          <div className={forgotPassword}>Don't have an account? Sign Up</div>
+        </form>
+      )}
       <ToastContainer position="top-center" />
     </div>
   );
