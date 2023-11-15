4e092a4692d3f8020661eb53e29080eec6a67a1c
Ayroid <ayushsk0000@gmail.com>
2023-11-15 06:58:48 +0530

    PROFILE SECTION IN PROGRESS

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,10 +2,12 @@ import Map from "./Map/Map";
 import Parkings from "./Parking/Parking";
 import Header from "./Header/Header";
 import Navigation from "./Navigation/Navigation";
+import UserProfile from "../UserProfile/UserProfile";
 
 const Home = () => {
   return (
     <div>
+      <UserProfile />
       <Header />
       <Navigation />
       <Map />
---
+++ src/components/UserProfile/UserProfile.jsx
@@ -0,0 +1,95 @@
+import styles from "./UserProfile.module.css";
+
+const UserProfile = () => {
+  // ---------------------------- CSS ----------------------------
+
+  const mainDiv = [styles.mainDiv].join("");
+  const backArrow = [styles.backArrow].join("");
+  const backgroundDiv = [styles.backgroundDiv].join("");
+  const profileDiv = [styles.profileDiv].join("");
+  const profilePic = [styles.profilePic].join("");
+  const profileData = [styles.profileData].join("");
+  const profileName = [styles.profileName].join("");
+  const profileEmail = [styles.profileEmail].join("");
+  const profileSAPID = [styles.profileSAPID].join("");
+  const contentDiv = [styles.contentDiv].join("");
+  const carsDiv = [styles.carsDiv].join("");
+  const cars = [styles.cars].join("");
+  const carImage = [styles.carImage].join("");
+  const carDetails = [styles.carDetails].join("");
+
+  // ---------------------------- JSX ----------------------------
+
+  return (
+    <div className={mainDiv}>
+      <div className={backArrow}>
+        <img src="public/icons/backArrow.png" alt="backArrow" />
+      </div>
+      <div className={backgroundDiv}></div>
+      <div className={profileDiv}>
+        <div className={profilePic}>
+          <img src="public/icons/profileicon.jpg" alt="profile icon" />
+        </div>
+        <div className={profileData}>
+          <div className={profileName}>Ayush Singh Kushwah</div>
+          <div className={profileEmail}>ayushsk0000@gmail.com</div>
+          <div className={profileSAPID}>500095575</div>
+        </div>
+      </div>
+      <div className={contentDiv}>
+        <div className={carsDiv}>
+          <div className={cars}>
+            <div className={carImage}>
+              <img src="public/icons/cars.jpg" alt="car.jpg" />
+            </div>
+            <div className={carDetails}>
+              NUMBER
+              <br />
+              MODEL
+              <br />
+              PARKING STATUS
+            </div>
+          </div>
+          <div className={cars}>
+            <div className={carImage}>
+              <img src="public/icons/cars.jpg" alt="car.jpg" />
+            </div>
+            <div className={carDetails}>
+              NUMBER
+              <br />
+              MODEL
+              <br />
+              PARKING SLOT
+            </div>
+          </div>
+          <div className={cars}>
+            <div className={carImage}>
+              <img src="public/icons/cars.jpg" alt="car.jpg" />
+            </div>
+            <div className={carDetails}>
+              NUMBER
+              <br />
+              MODEL
+              <br />
+              PARKING SLOT
+            </div>
+          </div>
+          <div className={cars}>
+            <div className={carImage}>
+              <img src="public/icons/cars.jpg" alt="car.jpg" />
+            </div>
+            <div className={carDetails}>
+              NUMBER
+              <br />
+              MODEL
+              <br />
+              PARKING SLOT
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserProfile;
