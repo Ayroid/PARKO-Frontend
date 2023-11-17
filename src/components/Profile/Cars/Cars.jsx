dda90909dfb0bc07e19fe8bedc46ef1e49f8c6e4
Ayroid <ayushsk0000@gmail.com>
2023-11-17 06:26:02 +0530

    design improvements

--- src/components/Profile/Cars/Cars.jsx
+++ src/components/Profile/Cars/Cars.jsx
@@ -5,6 +5,7 @@ const Cars = ({ data }) => {
   // ---------------------------- CSS ----------------------------
 
   const carsDiv = [styles.carsDiv].join("");
+  const innerCarDiv = [styles.innerCarDiv].join("");
   const cars = [styles.cars].join("");
   const carParking = [styles.carParking].join("");
   const carDetails = [styles.carDetails].join("");
@@ -12,21 +13,26 @@ const Cars = ({ data }) => {
   // ---------------------------- JSX ----------------------------
 
   return (
-    <div className={carsDiv}>
-      {data.map((vehicle, index) => (
-        <div key={index} className={cars}>
-          <div className={carParking}>
-            <img src="public/icons/cars/greycar.png" alt="car.jpg" />
-            <p>{index}</p>
-          </div>
-          <div className={carDetails}>
-            <p>Vehicle No: {vehicle.vehicleNumber}</p>
-            <p>Brand: {vehicle.brand}</p>
-            <p>Model: {vehicle.model}</p>
-          </div>
+    <>
+      <div className={carsDiv}>
+        <h2>Your Vehicles</h2>
+        <div className={innerCarDiv}>
+          {data.map((vehicle, index) => (
+            <div key={index} className={cars}>
+              <div className={carParking}>
+                <img src="public/icons/cars/greycar.png" alt="car.jpg" />
+                <p>{index}</p>
+              </div>
+              <div className={carDetails}>
+                <p>Vehicle No: {vehicle.vehicleNumber}</p>
+                <p>Brand: {vehicle.brand}</p>
+                <p>Model: {vehicle.model}</p>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      </div>
+    </>
   );
 };
 
